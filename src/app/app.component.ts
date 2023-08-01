9bd529ae5de3d42c86ad808a7e700b539359d7fb
kedi <kedi@KDs-Macbook-Air-2.local>
2023-08-01 11:26:33 +0800

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,13 @@ import { TodoItem } from "./todoItem";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showComplete: boolean = false;
+
   title(title: any) {
     throw new Error('Method not implemented.');
   }
   private list = new TodoList("Bob", [
-    new TodoItem("Go for run", true),
+    new TodoItem("Go for run"),
     new TodoItem("Get flowers"),
     new TodoItem("collect tickets"),
   ]);
@@ -30,16 +32,21 @@ export class AppComponent {
   get itemCount() : number { 
     //lambda, 
     //.fileter(..), filters the items array to only include items that are not complete
-    return this.list.items.filter(item => !item.complete).length;
+    return this.list.items.filter(item => this.showComplete || !item.complete).length;
   }
 
   get items() : readonly TodoItem[]{
-    return this.list.items.filter(item => !item.complete);
+    // 显示
+    //The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.
+    //  it will return a new array containing all items in the list if this.showComplete is truthy, or only incomplete items if this.showComplete is falsy.
+    return this.list.items.filter(item => this.showComplete || !item.complete);
   }
   addItem(newItem: string) {
     if (newItem != "") {
       this.list.addItem(newItem);
     }
   }
+
+
 }
 
